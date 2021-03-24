fe36522d99712bb9b90dd2002a4e9738327b8ad3
catsbi <killfine198@gmail.com>
2021-03-24 15:35:44 +0900

    Initial commit

---
+++ src/main/java/catsbi/me/penitta/PenittaApplication.java
@@ -0,0 +1,13 @@
+package catsbi.me.penitta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenittaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PenittaApplication.class, args);
+    }
+
+}
